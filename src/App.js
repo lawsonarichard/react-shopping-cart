e20336f97be055d2d193572a09b46827a2fda951
Richard Lawson <lawson.a.richard@gmail.com>
2019-08-13 12:42:06 -0700

    updated nav
    updated nav location

--- src/App.js
+++ src/App.js
@@ -18,13 +18,13 @@ function App() {
 
   return (
     <div className="App">
-      <ProductContext.Provider value={{ products, addItem }}>
-        <Route exact path="/" component={Products} />
-      </ProductContext.Provider>
       <CartContext.Provider value={{ cart }}>
         <Navigation />
         <Route path="/cart" component={ShoppingCart} />
       </CartContext.Provider>
+      <ProductContext.Provider value={{ products, addItem }}>
+        <Route exact path="/" component={Products} />
+      </ProductContext.Provider>
     </div>
   );
 }
